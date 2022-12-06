7b5c1d445a2f9a5ff3721ed81e3190c5e9eebee5
Dmitry <mazarini2002@yandex.ru>
2022-12-06 17:10:45 +0500

    Made a page about the company and offers and contacts

--- app/js/Jq-script.js
+++ app/js/Jq-script.js
@@ -41,21 +41,45 @@ $(document).ready(function() {
         }
     });
 
-    $('.applications__select').on('click', '.applications__select-item', function() {
-        $('.applications__select-head').removeClass('open');
+    $('.contacts__select').on('click', '.contacts__select-item', function() {
+        $('.contacts__select-head').removeClass('open');
         $(this).parent().fadeOut();
         $(this).parent().prev().text($(this).text());
         $(this).parent().prev().prev().val($(this).text());
     });
 
     $(document).click(function(e) {
-        if (!$(e.target).closest('.applications__select').length) {
-            $('.applications__select-head').removeClass('open');
-            $('.applications__select-list').fadeOut();
+        if (!$(e.target).closest('.contacts__select').length) {
+            $('.contacts__select-head').removeClass('open');
+            $('.contacts__select-list').fadeOut();
         }
     });
 
+    $('.contacts__select').on('click', '.contacts__select-head', function() {
+        if ($(this).hasClass('open')) {
+            $(this).removeClass('open');
+            $(this).next().fadeOut();
+        } else {
+            $('.contacts__select-head').removeClass('open');
+            $('.contacts__select-list').fadeOut();
+            $(this).addClass('open');
+            $(this).next().fadeIn();
+        }
+    });
 
+    $('.contacts__select').on('click', '.contacts__select-item', function() {
+        $('.contacts__select-head').removeClass('open');
+        $(this).parent().fadeOut();
+        $(this).parent().prev().text($(this).text());
+        $(this).parent().prev().prev().val($(this).text());
+    });
+
+    $(document).click(function(e) {
+        if (!$(e.target).closest('.contacts__select').length) {
+            $('.contacts__select-head').removeClass('open');
+            $('.contacts__select-list').fadeOut();
+        }
+    });
 
 
 
